0ff57ab085308d49c473446b03e87053154ada14
alexandramunoz <amunoz@ufl.edu>
2016-02-02 18:46:00 -0500

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Alexandra Munoz on 2/2/16.
+//  Copyright © 2016 Alexandra Munoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
